18ceb4aeb707b126da577edc68d7d6589e40d374
kidus-yo <kidusyonas96@gmail.com>
2026-09-03 15:22:19 +0300

    Updated

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
+from menu import *
 
-def main_menu():
- print("=" * 30)
- print("")
- print("File Organizer" )
- print()
- print("=" * 30)
- print()
- print("-" * 30)
- print("Main Menu ")
- print("-" * 30)
+def main():
+    choice = main_menu()
+    if choice == 1:
+        print("Welcome to File Organizer")
+        print("-"  * 30)
+        pass
 
- print("1. Organize Folder")
- print("2. View Files")
- print("3. View File Catagories")
- print("4. Organize by extension")
- print("5. Search Files")
- print("6. View Folder Statstics")
- try:
-    choice = int(input("Enter your choice: "))
-    return choice 
- except ValueError:
-   print("Enter the required inputs only!")
- except Exception:
-   print("Something went Wrong!")
+
+
+
+
+if __name__ == "__mian__":
+    main()
